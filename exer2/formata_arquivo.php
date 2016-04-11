8dc5c261f2d219bbe209238d9529ff7825f9652a
Willian Serafini <willian@onehost.com.br>
2016-04-10 21:16:11 -0300

    feito exercicio 2

---
+++ exer2/formata_arquivo.php
@@ -0,0 +1,35 @@
+<?php
+
+if (isset($_FILES['entrada']) && !empty($_FILES['entrada'])) { // se upou um arquivo
+    $arquivo = $_FILES['entrada'];
+    
+    try {
+        if (!move_uploaded_file($arquivo['tmp_name'], 'tmp/arquivo_formatado.txt')) { // tenta mover o arquivo para a pasta selecionada
+            throw new Exception('Não foi possível mover o arquivo!');
+        }
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+    }
+
+    $arquivoUpado = 'tmp/arquivo_formatado.txt';
+    $linhasDoArquivo = file($arquivoUpado); // coloca as linhas do txt em um array
+    $arquivoFormatado = '';
+    foreach ($linhasDoArquivo as $linha) {
+        $expressaoRegular = '/  /'; // procurar na linha por 2 espaços
+        $trocarPor = ' '; // troca por 1 espaço
+        $arquivoFormatado .= preg_replace($expressaoRegular, $trocarPor, $linha); // procura por 2 espaços na linha e troca por 1 se achar
+        $arquivoFormatado .= "\n"; // nova linha
+    }
+
+    file_put_contents($arquivoUpado, $arquivoFormatado); // coloca no arquivo o texto formatado
+    
+    //linhas abaixo servem para forçar o download do arquivo formatado .txt
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename=' . basename($arquivoUpado));
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($arquivoUpado));
+    readfile($arquivoUpado);
+}
+?>
---
+++ exer2/index.php
@@ -0,0 +1,25 @@
+<?php
+//Grupo: Willian Serafini, Gabriel Calderaro e Samantha Machado
+/*
+ * 2) Escreva um programa em LEX que copie um arquivo, 
+ * substituindo cada sequência vazia de espaço em branco por um único espaço.
+ */
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+        <title>Exercicío Aula 5 de Compiladores</title>
+    </head>
+
+    <body>
+        <div style="margin: 0 auto; width: 600px;">
+            <h2>Exercicío Aula 5 de Compiladores</h2><br/><br/>
+            <form action="formata_arquivo.php" method="post" enctype="multipart/form-data">
+                <label>Arquivo(.txt):</label><input name="entrada" type="file" /><br/><br/>
+                <input type="submit" value="Gerar Arquivo Formatado" />
+            </form><br/><br/>
+        </div>
+    </body>
+</html>
